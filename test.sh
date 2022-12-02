65041ffef36165976d8557c0f2fb5931921862b2
Miguel Fonseca <miguelcsf@gmail.com>
2022-12-02 18:48:12 +0000

    Initial commit

---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+test_day() {
+	(
+		day=$1
+		cd "$day" || exit 2
+		echo "$day"
+		for runner in run*; do
+			if [ -x "$runner" ]; then
+				printf "  %s\t%s\n" "$runner" "$(pass_or_fail "$runner")"
+			else
+				printf "  %s\tnot executable\n" "$runner"
+			fi
+		done
+	)
+}
+
+pass_or_fail() {
+	runner="$1"
+	if "./$runner" < input | diff - expected; then
+		echo PASS
+	else
+		echo FAIL
+	fi
+}
+
+for day in */; do
+	if [ -f "$day"/expected ]; then
+		test_day "$day"
+	else
+		printf "%s\n  (skipped)\n" "$day"
+	fi
+done
